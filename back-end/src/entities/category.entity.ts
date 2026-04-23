b1f4ae627bfca65fa37a00eecf52eeb44b304b79
pedroanastacio <pedro.anastacio013@gmail.com>
2026-04-23 10:04:08 -0300

    feat: create category and product entities

---
+++ back-end/src/entities/category.entity.ts
@@ -0,0 +1,60 @@
+export interface CategoryProps {
+  id?: string;
+  name: string;
+  description?: string | null;
+  parentId?: string | null;
+}
+
+export class Category {
+  private _id?: string;
+  private _name!: string;
+  private _description?: string | null;
+  private _parentId?: string | null;
+
+  constructor(props: CategoryProps) {
+    this.validate(props);
+
+    this._id = props.id;
+    this._name = props.name;
+    this._description = props.description ?? null;
+    this._parentId = props.parentId ?? null;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get parentId() {
+    return this._parentId;
+  }
+
+  validateHierarchy(ancestors: Category[]) {
+    if (!this._id) return;
+
+    const relatedToItself = ancestors.some(
+      (ancestor) => ancestor.id === this._id,
+    );
+
+    if (relatedToItself) {
+      throw new Error('Category cannot be related to itself');
+    }
+  }
+
+  private validate(props: CategoryProps) {
+    if (!props.name) {
+      throw new Error('Category name is required');
+    }
+
+    if (props.parentId && props.id && props.parentId === props.id) {
+      throw new Error('Category cannot be related to itself');
+    }
+  }
+}
---
+++ back-end/src/entities/product.entity.ts
@@ -0,0 +1,90 @@
+export interface ProductProps {
+  id?: string;
+  name: string;
+  description?: string | null;
+  price: number;
+  categoryIds: string[];
+}
+
+export class Product {
+  private _id?: string;
+  private _name!: string;
+  private _description?: string | null;
+  private _price!: number;
+  private _categoryIds!: string[];
+
+  constructor(props: ProductProps) {
+    this.validate(props);
+
+    this._id = props.id;
+    this._name = props.name;
+    this._description = props.description ?? null;
+    this._price = props.price;
+    this._categoryIds = props.categoryIds;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  get categoryIds() {
+    return this._categoryIds;
+  }
+
+  update(data: Partial<Omit<ProductProps, 'id'>>) {
+    if (data.name !== undefined) {
+      this.checkName(data.name);
+      this._name = data.name;
+    }
+
+    if (data.description !== undefined) {
+      this._description = data.description;
+    }
+
+    if (data.price !== undefined) {
+      this.checkPrice(data.price);
+      this._price = data.price;
+    }
+
+    if (data.categoryIds !== undefined) {
+      this.checkCategoryIds(data.categoryIds);
+      this._categoryIds = data.categoryIds;
+    }
+  }
+
+  private checkName(name: string) {
+    if (!name || name.trim() === '') {
+      throw new Error('Product name is required');
+    }
+  }
+
+  private checkPrice(price: number) {
+    if (price < 0) {
+      throw new Error('Product price cannot be negative');
+    }
+  }
+
+  private checkCategoryIds(categoryIds: string[]) {
+    if (!categoryIds || categoryIds.length === 0) {
+      throw new Error('Product must belong to at least one category');
+    }
+  }
+
+  private validate(props: ProductProps) {
+    this.checkName(props.name);
+    this.checkPrice(props.price);
+    this.checkCategoryIds(props.categoryIds);
+  }
+}
