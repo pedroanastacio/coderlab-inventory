307efe0c6e1981045dd1c6723d9938038f8e76fa
pedroanastacio <pedro.anastacio013@gmail.com>
2026-04-27 15:51:57 -0300

    chore: remove unnecessary prisma service provider

--- back-end/src/infra/modules/category.module.ts
+++ back-end/src/infra/modules/category.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../database/prisma/prisma.service';
-import { PrismaCategoryRepository } from '../database/prisma/repositories/prisma-category.repository';
 import { CreateCategoryUseCase } from '../../application/use-cases/create-category.use-case';
 import { FindCategoryByIdUseCase } from '../../application/use-cases/find-category-by-id.use-case';
 import { FindAllCategoriesUseCase } from '../../application/use-cases/find-all-categories.use-case';
 import { UpdateCategoryUseCase } from '../../application/use-cases/update-category.use-case';
-import { CATEGORY_REPOSITORY } from '../../domain/repositories/tokens';
 import { CategoryController } from '../http/controllers/category.controller';
+import { CATEGORY_REPOSITORY } from '../../domain/repositories/tokens';
+import { PrismaCategoryRepository } from '../database/prisma/repositories/prisma-category.repository';
 
 @Module({
   providers: [
-    PrismaService,
     {
       provide: CATEGORY_REPOSITORY,
       useClass: PrismaCategoryRepository,
