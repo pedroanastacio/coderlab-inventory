3385829f95edd518d2a1cc3a77b849cd98f71c30
pedroanastacio <pedro.anastacio013@gmail.com>
2026-05-11 16:01:46 -0300

    fix: enable Suspense and throwOnError in QueryClient config

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
+import './lib/axios-setup';
 import './index.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      throwOnError: true,
       retry: 1,
       refetchOnWindowFocus: false,
     },
